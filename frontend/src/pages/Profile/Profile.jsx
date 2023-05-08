c57d10c370720c2d90cfa6605d8a5edf9feffca0
Sahad <sahad@riafy.me>
2023-05-08 18:01:06 +0530

    f- add theme change btn

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import {FlatCard} from '../../components';
 import './Profile.css'
 
 const Profile = () => {
+  const handleThemeChange = () => {
+    document.body.classList.toggle("theme-dark-green")
+  }
+
+  const style = {
+    padding: "1rem",
+    margin: "3rem"
+  }
+  
+  
   return (
     <div className='page'>
-      Profile
+      <button style={style}  onClick={handleThemeChange}>Theme Change</button>
     </div>
   );
 }
