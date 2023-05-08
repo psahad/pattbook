232f1e6ce251d0914e931065d51ba0d24ff0b965
Sahad <sahad@riafy.me>
2023-05-08 13:26:56 +0530

    f- fonts add, add pg basic

--- frontend/src/pages/Add/Add.jsx
+++ frontend/src/pages/Add/Add.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
-import './Add.css'
+// import React from 'react';
+// import './Add.css'
 
-const Add = () => {
+// const Add = () => {
+//   return (
+//     <div className='page'>
+//       Add
+//     </div>
+//   );
+// }
+
+// export default Add;
+
+import React, { useState } from 'react';
+
+function Add() {
+  const [contacts, setContacts] = useState([]);
+
+  const handleClick = async () => {
+    if ('contacts' in navigator) {
+      try {
+        const contactsData = await navigator.contacts.select(['name', 'phoneNumbers']);
+        setContacts(contactsData);
+      } catch (err) {
+        console.error(err);
+      }
+    } else {
+      console.log('Contacts API not supported');
+    }
+  };
+
   return (
-    <div className='page'>
-      Add
+    <div>
+      <button onClick={handleClick}>Fetch Contacts</button>
+      {contacts.length > 0 && (
+        <ul>
+          {contacts.map((contact) => (
+            <li key={contact.id}>
+              <strong>Name:</strong> {contact.name.formatted} <br />
+              <strong>Phone:</strong> {contact.phoneNumbers[0].value}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
 
 export default Add;
+
