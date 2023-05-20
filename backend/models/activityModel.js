5867be5664cd7ee9809fd4a16f56bdef0cfda2df
Sahad <sahad@riafy.me>
2023-05-20 14:34:12 +0530

    b- user and activity schema

--- backend/models/activityModel.js
+++ backend/models/activityModel.js
@@ -1,15 +1,40 @@
 const mongoose = require("mongoose");
 
 const activitySchema = mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["lend", "borrow"],
+    default: "lend"
+  },
   from: {
     type: mongoose.Types.ObjectId,
     required: [true, "User from is required"],
     ref: "User",
   },
+  to: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "User to is required"],
+    ref: "User",
+  },
   amount: {
     type: Number,
+    min: [1, "Amount must be greater than zero"],
     required: [true, "Amount is required"],
   },
+  purpose: {
+    type: String
+  },
+  returnExpectedAt: {
+    type: Date
+  },
+  status: {
+    type: String,
+    enum: ["request", "open", "close"],
+    default: "open"
+  },
+  requestedAt: Date,
+  openAt: Date,
+  closeAt: Date
 });
 
 module.exports = mongoose.model("Activity", activitySchema);
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,20 +1,47 @@
 const mongoose = require('mongoose')
 
+const settingsSchema = mongoose.Schema({
+  fontSize: {
+    type: String,
+    enum: ["small", "normal", "large"],
+    default: "normal",
+  },
+  language: {
+    type: String,
+    default: "english",
+  },
+  language: {
+    type: String,
+    default: "primaryLight",
+  },
+}) 
+
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
+      minLength: [3, 'Name must have a minimum length of 3 characters'],
+      maxLength: [50, 'Name must have a maximum length of 50 characters'],
       required: [true, 'Please add a name'],
     },
     phone: {
       type: Number,
-      required: [true, 'Please add an email'],
+      required: [true, 'Please add an phone number'],
       unique: true,
     },
     password: {
       type: String,
       required: [true, 'Please add a password'],
     },
+    settings: {
+      type: settingsSchema
+    },
+    total_lending: {
+      type: Number
+    },
+    total_borrowing: {
+      type: Number
+    }
   },
   {
     timestamps: true,
