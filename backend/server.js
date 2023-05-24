ce5c16143088510a344593bb70831f13f8bbad31
psahad <sahad@riafy.me>
2023-05-24 09:47:22 +0530

    b- setup cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const path = require("path")
 const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const {errorHandler} = require("./middleware/errorMiddleware")
 const connectDB = require("./config/db")
 const port = process.env.PORT || 5000;
@@ -12,6 +13,9 @@ const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors({
+  origin: "http://127.0.0.1:3000"
+}));
 
 app.use("/api/user", require("./routes/userRoutes"))
 app.use("/api/activity", require("./routes/activityRoutes"))
