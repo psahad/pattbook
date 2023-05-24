19c2cfe5e90968edaff98be7a1069e6add04bea7
psahad <sahad@riafy.me>
2023-05-24 09:54:15 +0530

    b- change cors origin to all

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
-  origin: "http://127.0.0.1:3000"
+  origin: "*"
 }));
 
 app.use("/api/user", require("./routes/userRoutes"))
