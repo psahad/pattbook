77709f58c62af7966593fc6edbebdf71ee1ebca9
psahad <sahad@riafy.me>
2023-05-24 17:57:00 +0530

    b- status change for error middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,10 +1,12 @@
 const errorHandler = (err, req, res, next) => {
-  const statusCode = res.statusCode ?? 500;
+  let statusCode = res.statusCode ?? 500;
+
+  if(Math.floor(statusCode / 100) === 2) statusCode = 400
 
   res.status(statusCode);
 
   res.json({
-    message: err.message,
+    error: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 };
