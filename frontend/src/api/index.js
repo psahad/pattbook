bd0c27d799a82a0be36d103308c917d640701d70
psahad <sahad@riafy.me>
2023-05-25 13:11:25 +0530

    b- adjsutmets

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,7 @@
-import {put, post, get, del} from "./networkUtils";
+import {post} from "./networkUtils";
 
 // const BASE_URL = process.env.API_BASE_URL;
+// const BASE_URL = "http://localhost:5000";
 const BASE_URL = "https://pattbook.onrender.com";
 
 // Register user
